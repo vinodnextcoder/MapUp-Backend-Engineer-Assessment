8d930b9130c97c4fa0222aebe38130ca4b81a980
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-10-25 23:23:40 +0530

    authorazation api key added

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -5,7 +5,7 @@ var VerifyToken = require('./VerifyToken');
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-router.get('/me', VerifyToken, function (req, res, next) {
+router.post('/mapUserTest', VerifyToken, function (req, res, next) {
   if (err) return res.status(500).send("There was a problem finding the user.");
   if (!user) return res.status(404).send("No user found.");
   res.status(200).send(user);
