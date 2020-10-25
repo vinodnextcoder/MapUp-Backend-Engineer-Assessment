1bd6e78d5f2a41e68f53b19b5a90fa8296f0993d
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-10-26 00:41:58 +0530

    turf json sample added
    line and lon line js added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 global.__root   = __dirname + '/'; 
+global.__root = __dirname + '/';
+app.set('view engine', 'ejs');
 app.get('/api', function (req, res) {
   res.status(200).send('API works.');
+});
+app.get('/', function (req, res) {
+  // res.status(200).send('API works.');
+  res.render('demo'); 
 });
 var AuthController = require(__root + 'auth/AuthController');
 app.use('/api/auth', AuthController);
--- auth/AuthController.js
+++ auth/AuthController.js
@@ -5,12 +5,23 @@ var VerifyToken = require('./VerifyToken');
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 var turf = require('@turf/turf');
+var line1=require("../jsonInput/lines.json");
+var line2=require("../jsonInput/long-ls.json")
 
 router.post('/mapUserTest', VerifyToken, function (req, res, next) {
-  var line1 = turf.lineString([[126, -11], [129, -21]]);
-  var line2 = turf.lineString([[123, -18], [131, -14]]);
+  // line1= JSON.stringify(line1);
+  // line2= JSON.stringify(line2);
+  var LineInterect = {
+    "type": "FeatureCollection",
+    "features": [line1, line2]
+  };
+  // var intersection = turf.intersect(line1, line2);
   var intersects = turf.lineIntersect(line1, line2);
   res.status(200).send(intersects);
+  // var line1 = turf.lineString([[126, -11], [129, -21]]);
+  // var line2 = turf.lineString([[123, -18], [131, -14]]);
+  // var intersects = turf.lineIntersect(line1, line2);
+  // res.status(200).send(intersects);
 });
 
 module.exports = router;
