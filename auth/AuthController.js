0c18d55ca15c42ddb612735a484d2a0e1c1068a3
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-10-25 23:32:37 +0530

    api key added

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -6,9 +6,9 @@ router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
 router.post('/mapUserTest', VerifyToken, function (req, res, next) {
-  if (err) return res.status(500).send("There was a problem finding the user.");
-  if (!user) return res.status(404).send("No user found.");
-  res.status(200).send(user);
+  // if (err) return res.status(500).send("There was a problem finding the user.");
+  // if (!user) return res.status(404).send("No user found.");
+  res.status(200).send({});
 });
 
 module.exports = router;
--- auth/VerifyToken.js
+++ auth/VerifyToken.js
@@ -2,14 +2,14 @@ var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('../config'); // get our config file
 function verifyToken(req, res, next) {
   // check header or url parameters or post parameters for token
-  var token = req.headers['x-access-token'];
+  var token = req.headers['authorization'];
   if (!token)
     return res.status(403).send({ auth: false, message: 'No token provided.' });
-  if (token === "zNXT3yFA") {
-    return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+  if (token == "zNXT3yFA") {
+    next();
   }
   else {
-    next();
+    return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
   }
 }
 
