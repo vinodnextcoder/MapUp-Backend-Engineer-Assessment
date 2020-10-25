99f0ab16a154131140becaec070559da84ca739f
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-10-26 00:05:15 +0530

    line of intersection

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -7,50 +7,10 @@ router.use(bodyParser.json());
 var turf = require('@turf/turf');
 
 router.post('/mapUserTest', VerifyToken, function (req, res, next) {
-  var poly1 = {
-    "type": "Feature",
-    "properties": {
-      "fill": "#0f0"
-    },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [[
-        [-122.801742, 45.48565],
-        [-122.801742, 45.60491],
-        [-122.584762, 45.60491],
-        [-122.584762, 45.48565],
-        [-122.801742, 45.48565]
-      ]]
-    }
-  }
-  var poly2 = {
-    "type": "Feature",
-    "properties": {
-      "fill": "#00f"
-    },
-    "geometry": {
-      "type": "Polygon",
-      "coordinates": [[
-        [-122.520217, 45.535693],
-        [-122.64038, 45.553967],
-        [-122.720031, 45.526554],
-        [-122.669906, 45.507309],
-        [-122.723464, 45.446643],
-        [-122.532577, 45.408574],
-        [-122.487258, 45.477466],
-        [-122.520217, 45.535693]
-      ]]
-    }
-  }
-  
-  var polygons = {
-    "type": "FeatureCollection",
-    "features": [poly1, poly2]
-  };
-  
-  var intersection = turf.intersect(poly1, poly2);
-  console.log(">>>>>>turf",intersection)
-  res.status(200).send(intersection);
+  var line1 = turf.lineString([[126, -11], [129, -21]]);
+  var line2 = turf.lineString([[123, -18], [131, -14]]);
+  var intersects = turf.lineIntersect(line1, line2);
+  res.status(200).send(intersects);
 });
 
 module.exports = router;
