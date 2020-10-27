8b48cbf23a289ccf77bac09b666a7fa5080d6f74
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-10-28 01:06:01 +0530

    intesect find call function

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -7,13 +7,16 @@ router.use(bodyParser.json());
 var turf = require('@turf/turf');
 // var line1=require("../jsonInput/lines.json");
 // var line2=require("../jsonInput/long-ls.json")
+// middleware added for auth with api key
 
-router.post('/mapUserTest', VerifyToken, function (req, res, next) {
+const turfFindIntersectLineString = (req, res, next) => {
+  // validate request body
   if ((typeof req.body.line1 !== 'undefined' && req.body.line1.length > 0) && (typeof req.body.line2 !== 'undefined' && req.body.line2.length > 0)) {
     var line1 = turf.lineString(req.body.line1);
     var line2 = turf.lineString(req.body.line2);
     var intersects = turf.lineIntersect(line1, line2);
     if (intersects && intersects.features.length==0){
+      // no tersect found error
       res.status(400).send({ "msg": "No intersect found" });
     }
     else{
@@ -23,6 +26,6 @@ router.post('/mapUserTest', VerifyToken, function (req, res, next) {
   else {
     res.status(400).send({ "msg": "invalid input" });
   }
-});
-
+}
+router.post('/mapUserTest', VerifyToken , turfFindIntersectLineString)
 module.exports = router;
--- auth/VerifyToken.js
+++ auth/VerifyToken.js
@@ -1,6 +1,8 @@
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('../config'); // get our config file
-function verifyToken(req, res, next) {
+// middleware auth function
+
+let verifyToken = (req, res, next) => {
   // check header or url parameters or post parameters for token
   var token = req.headers['authorization'];
   if (!token)
@@ -11,6 +13,7 @@ function verifyToken(req, res, next) {
   else {
     return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
   }
+
 }
 
 module.exports = verifyToken;
