ebb5a2c45072732605bc0d6bb6c87b2a9d04007a
vinod <31404750+vinodnextcoder@users.noreply.github.com>
2020-10-28 00:57:13 +0530

    linestring intersect in turf

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -5,23 +5,24 @@ var VerifyToken = require('./VerifyToken');
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 var turf = require('@turf/turf');
-var line1=require("../jsonInput/lines.json");
-var line2=require("../jsonInput/long-ls.json")
+// var line1=require("../jsonInput/lines.json");
+// var line2=require("../jsonInput/long-ls.json")
 
 router.post('/mapUserTest', VerifyToken, function (req, res, next) {
-  // line1= JSON.stringify(line1);
-  // line2= JSON.stringify(line2);
-  var LineInterect = {
-    "type": "FeatureCollection",
-    "features": [line1, line2]
-  };
-  // var intersection = turf.intersect(line1, line2);
-  var intersects = turf.lineIntersect(line1, line2);
-  res.status(200).send(intersects);
-  // var line1 = turf.lineString([[126, -11], [129, -21]]);
-  // var line2 = turf.lineString([[123, -18], [131, -14]]);
-  // var intersects = turf.lineIntersect(line1, line2);
-  // res.status(200).send(intersects);
+  if ((typeof req.body.line1 !== 'undefined' && req.body.line1.length > 0) && (typeof req.body.line2 !== 'undefined' && req.body.line2.length > 0)) {
+    var line1 = turf.lineString(req.body.line1);
+    var line2 = turf.lineString(req.body.line2);
+    var intersects = turf.lineIntersect(line1, line2);
+    if (intersects && intersects.features.length==0){
+      res.status(400).send({ "msg": "No intersect found" });
+    }
+    else{
+      res.status(200).send(intersects);
+    }
+  }
+  else {
+    res.status(400).send({ "msg": "invalid input" });
+  }
 });
 
 module.exports = router;
